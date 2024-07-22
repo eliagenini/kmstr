0b47630a40330128e754a303d6d727f04a8dc4b5
Genini Elia <elia.genini@ti.ch>
2024-07-22 18:40:01 +0200

    Copying trip_agent

---
+++ importer/importer/models/trip.py
@@ -0,0 +1,19 @@
+from weconnect.addressable import AddressableLeaf
+
+class Trip:
+
+    def __init__(self, data):
+        self.id = data.get('id')
+        self.vehicle = data.get('vehicle')
+        self.date = {'start': data.get('start_date'), 'end': data.get('end_date')}
+        self.position = {
+            'start': Position(data.get('start_position_latitude'), data.get('start_position_longitude')),
+            'end': Position(data.get('end_position_latitude'), data.get('end_position_longitude'))}
+        self.mileage = {'start': data.get('start_mileage'), 'end': data.get('end_mileage')}
+        self.last_modified = data.get('last_modified')
+
+
+class Position:
+    def __init__(self, lat, long):
+        self.latitude = lat
+        self.longitude = long
