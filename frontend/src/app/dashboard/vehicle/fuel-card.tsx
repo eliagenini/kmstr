4e9240da983ffca7f63890f2e18140cc8b13e6de
Genini Elia <elia.genini@ti.ch>
2024-07-29 17:19:44 +0200

    with fooking charts!!!

--- frontend/src/app/dashboard/vehicle/fuel-card.tsx
+++ frontend/src/app/dashboard/vehicle/fuel-card.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Card,
     CardContent,
@@ -7,7 +9,30 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+
+import { TrendingUp } from "lucide-react"
+import { Label, PolarRadiusAxis, RadialBar, RadialBarChart } from "recharts"
+
+const chartConfig = {
+    desktop: {
+        label: "Desktop",
+        color: "hsl(var(--chart-1))",
+    },
+    mobile: {
+        label: "Mobile",
+        color: "hsl(var(--chart-2))",
+    },
+} satisfies ChartConfig
+
 export function FuelCard({ fuel }: { fuel }) {
+    const total = 100;
+    const chartData = [{ month: "january", available: fuel, max: (100 - fuel) }]
 
     return (
         <Card>
@@ -18,10 +43,62 @@ export function FuelCard({ fuel }: { fuel }) {
 
             </CardHeader>
             <CardContent>
-                <div className="text-2xl font-bold">{fuel} %</div>
-                <p className="text-xs text-muted-foreground">
-                    +20.1% from last month
-                </p>
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square w-full max-w-[250px]"
+                >
+                    <RadialBarChart
+                        data={chartData}
+                        endAngle={180}
+                        innerRadius={80}
+                        outerRadius={130}
+                    >
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                            <Label
+                                content={({ viewBox }) => {
+                                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                        return (
+                                            <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle">
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) - 16}
+                                                    className="fill-foreground text-2xl font-bold"
+                                                >
+                                                    {fuel.toLocaleString()}
+                                                </tspan>
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) + 4}
+                                                    className="fill-muted-foreground"
+                                                >
+                                                    %
+                                                </tspan>
+                                            </text>
+                                        )
+                                    }
+                                }}
+                            />
+                        </PolarRadiusAxis>
+                        <RadialBar
+                            dataKey="max"
+                            stackId="a"
+                            cornerRadius={5}
+                            fill="var(--color-desktop)"
+                            className="stroke-transparent stroke-2"
+                        />
+                        <RadialBar
+                            dataKey="available"
+                            fill="var(--color-mobile)"
+                            stackId="a"
+                            cornerRadius={5}
+                            className="stroke-transparent stroke-2"
+                        />
+                    </RadialBarChart>
+                </ChartContainer>
             </CardContent>
         </Card>
     )
